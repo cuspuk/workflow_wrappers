5677849562c31d6e4390a2454cbfa3917673cb9a
Jozef Sitarcik <jozefsitarcik@gmail.com>
2024-06-05 09:20:41 +0200

    fix: decontamination now allows to exclude/include

--- wrappers/kraken/decontaminate_pe/wrapper.py
+++ wrappers/kraken/decontaminate_pe/wrapper.py
@@ -3,6 +3,9 @@ from snakemake.shell import shell
 log = snakemake.log_fmt_shell(stdout=False, stderr=True)
 
 extra = snakemake.params.get("extra", "")
+exclude_flag = snakemake.params.get("exclude", True)
+if exclude_flag:
+    extra += " --exclude"
 
 temp_r1 = snakemake.output.r1.removesuffix(".gz")
 temp_r2 = snakemake.output.r2.removesuffix(".gz")
@@ -12,7 +15,7 @@ shell(
     " extract_kraken_reads.py"
     " -k {snakemake.input.kraken_output} -r {snakemake.input.kraken_report}"
     " -s {snakemake.input.r1} -s2 {snakemake.input.r2} -o {temp_r1} -o2 {temp_r2}"
-    " --exclude -t {snakemake.params.taxid} {extra} --fastq-output > {snakemake.output.std_out}"
+    " -t {snakemake.params.taxid} {extra} --fastq-output > {snakemake.output.std_out}"
     " &&"
     " pigz {temp_r1} -9 -c -p {snakemake.threads} > {snakemake.output.r1}"
     " && "
